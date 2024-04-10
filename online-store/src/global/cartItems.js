82753b8ab215f9014e49f2b972dbfc401f01f992
Braydon Tol <braydontol@gmail.com>
2024-04-10 15:04:00 -0600

    remove some nonsense

--- online-store/src/global/cartItems.js
+++ online-store/src/global/cartItems.js
@@ -11,21 +11,6 @@ import { writable } from 'svelte/store';
 
 let storedItems = [];
 
-if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
-  // use localStorage here
-  const cartItemsJSON = localStorage.getItem('cartItems');
-  if (cartItemsJSON) {
-    try {
-      storedItems = JSON.parse(cartItemsJSON);
-    } catch (e) {
-      console.error('Error parsing stored cart items:', e);
-    }
-  }
-} else {
-  // localStorage is not available
-  console.error('localStorage is not available');
-}
-
 const cartItems = writable(storedItems);
 
 // Subscribe to the cartItems store and save the current items to localStorage
